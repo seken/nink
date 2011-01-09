87d0ccc4d5fb08e4f8f3afb3c464489f05230843
Mark Goodall <markit@seken.co.uk>
2011-01-09 20:38:43 +0000

    Added license

--- collisionMap.py
+++ collisionMap.py
@@ -1,3 +1,17 @@
+# This file is part of Nink.
+# 
+# Nink is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Nink is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Nink.  If not, see <http://www.gnu.org/licenses/>.
 class CollisionMap(object):
 	def __init__(self, bitmap, minc, maxc, dimc):
 		super(CollisionMap, self).__init__()
